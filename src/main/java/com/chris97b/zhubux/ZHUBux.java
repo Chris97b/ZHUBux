5334f973ac0fbfd7f12f7a602198cac76860dbcd
Chris97b <admin@chris97b.com>
2015-10-29 05:23:14 -0500

    Setup basic framework

---
+++ src/main/java/com/chris97b/zhubux/ZHUBux.java
@@ -0,0 +1,35 @@
+package com.chris97b.zhubux;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Chris on 10/29/2015.
+ */
+
+@Mod(modid="ZHUBux", name= "ZHU Bux", version="1.7.10-1.0")
+public class ZHUBux
+{
+    @Mod.Instance("ZHUBux")
+    public static ZHUBux instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
